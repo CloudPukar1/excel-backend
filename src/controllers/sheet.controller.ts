a016fef0fc4f44bcb386fffa2e0d4433d30afeae
Rocky Pukar <cloudpukar@gmail.com>
2025-03-20 13:44:11 +0545

    feat: setup sheet model and controllers for sheet

--- src/controllers/sheet.controller.ts
+++ src/controllers/sheet.controller.ts
@@ -1,21 +1,97 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import Sheet from "../models/sheet.model";
 
-export const createSheet = () => {};
+export const createSheet = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, userId, data } = req.body;
+
+    const sheet = new Sheet({
+      name,
+      userId,
+      data,
+    });
+    await sheet.save();
+    res.status(201).json({
+      success: true,
+      message: "Sheet created successfully",
+      data: sheet,
+    });
+  } catch (error) {
+    next({ message: "Error Creating sheet" });
+  }
+};
 
 export const getSheets = () => {};
 
-export const getSheet = (req: Request, res: Response) => {
-  res.json({
-    success: true,
-    message: "Sheet data fetched successfully",
-    data: [
-      ["12", "abc", "16"],
-      ["15", "12", "!@$#"],
-      ["15", "12", "!@$#", ""],
-    ],
-  });
+export const getSheet = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const sheet = await Sheet.findById(req.params.id);
+    if (sheet) {
+      next({ status: 404, message: "Sheet not found" });
+    }
+    res.json({
+      success: true,
+      message: "Sheet data fetched successfully",
+      data: sheet,
+    });
+  } catch (error) {
+    next({ message: "Error fetching sheet" });
+  }
 };
 
-export const updateSheet = () => {};
+export const updateSheet = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { data } = req.body;
 
-export const deleteSheet = () => {};
+    const sheet = await Sheet.findByIdAndUpdate(
+      req.params.id,
+      { data, updatedAt: Date.now() },
+      { new: true }
+    );
+
+    if (!sheet) {
+      return next({ status: 404, message: "Sheet not found" });
+    }
+
+    res.json({
+      success: true,
+      message: "Sheet data fetched successfully",
+      data: sheet,
+    });
+  } catch (error) {
+    next({ message: "Error updating sheet" });
+  }
+};
+
+export const deleteSheet = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const sheet = await Sheet.findByIdAndDelete(req.params.id);
+
+    if (!sheet) {
+      return next({ status: 404, message: "Sheet not found" });
+    }
+
+    res.json({
+      success: true,
+      message: "Sheet deleted successfully",
+    });
+  } catch (error) {
+    next({ message: "Error updating sheet" });
+  }
+};
---
+++ src/models/sheet.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const SheetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  data: {
+    type: [[String]],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Sheet", SheetSchema);
